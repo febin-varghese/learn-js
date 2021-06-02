998f807e432bc56ab9819a227e7ea8111f419a10
febin-varghese <febinvarghese994@gmail.com>
2021-06-02 10:03:14 +0200

    log created

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,34 +1,53 @@
 let currentResult = 0;
-
-function add(){
-    const inputNumber = parseInt(userInput.value);
-    const description = `${currentResult} + ${inputNumber}`;
-    currentResult = currentResult + inputNumber;
-    outputResult(currentResult, description);
+let logEntries = [];
+function writeLog(operator, previousValue, inputValue, newValue) {
+  const newEntry = {
+    operation: operator,
+    previousResult: previousValue,
+    enteredNumber: inputValue,
+    result: newValue,
+  };
+  logEntries.push(newEntry);
+  console.log(logEntries);
 }
 
-function subtract(){
-    const inputNumber = parseInt(userInput.value);
-    const description = `${currentResult} - ${inputNumber}`;
-    currentResult = currentResult - inputNumber;
-    outputResult(currentResult, description);
+function add() {
+  const inputNumber = parseInt(userInput.value);
+  const initialResult = currentResult;
+  const description = `${initialResult} + ${inputNumber}`;
+  currentResult = currentResult + inputNumber;
+  outputResult(currentResult, description);
+  writeLog("ADD", initialResult, inputNumber, currentResult);
 }
 
-function multiply(){
-    const inputNumber = parseInt(userInput.value);
-    const description = `${currentResult} * ${inputNumber}`;
-    currentResult = currentResult * inputNumber;
-    outputResult(currentResult, description);
+function subtract() {
+  const inputNumber = parseInt(userInput.value);
+  const initialResult = currentResult;
+  const description = `${initialResult} - ${inputNumber}`;
+  currentResult = currentResult - inputNumber;
+  outputResult(currentResult, description);
+  writeLog("SUBTRACT", initialResult, inputNumber, currentResult);
 }
 
-function divide(){
-    const inputNumber = parseInt(userInput.value);
-    const description = `${currentResult} / ${inputNumber}`;
-    currentResult = currentResult / inputNumber;
-    outputResult(currentResult, description);
+function multiply() {
+  const inputNumber = parseInt(userInput.value);
+  const initialResult = currentResult;
+  const description = `${initialResult} * ${inputNumber}`;
+  currentResult = currentResult * inputNumber;
+  outputResult(currentResult, description);
+  writeLog("MULTIPY", initialResult, inputNumber, currentResult);
 }
 
-addBtn.addEventListener('click', add);
-subtractBtn.addEventListener('click', subtract);
-multiplyBtn.addEventListener('click', multiply);
-divideBtn.addEventListener('click', divide);
+function divide() {
+  const inputNumber = parseInt(userInput.value);
+  const initialResult = currentResult;
+  const description = `${initialResult} / ${inputNumber}`;
+  currentResult = currentResult / inputNumber;
+  outputResult(currentResult, description);
+  writeLog("DIVIDE", initialResult, inputNumber, currentResult);
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
